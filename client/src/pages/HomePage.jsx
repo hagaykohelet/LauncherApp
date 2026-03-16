0aaf14bb521d431e0ac3f16a0e4c2b18903cf563
hagay kohelet <hagay2506@gmail.com>
2026-03-16 12:34:45 +0200

    home page with fetch data from rout

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,21 +1,41 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
-
+import '../style/HomePage.css'
 function HomePage() {
     const [launchers, setLaunchers] = useState([])
     async function getAllLaunchers() {
         const res = await fetch("http://localhost:3000/api/launchers")
         const data = await res.json()
-        setLaunchers(data)
+        setLaunchers(data.launchers)
     }
-    
-useEffect(() => {
-    getAllLaunchers()
-}, [])
-console.log(launchers)
+
+    useEffect(() => {
+        getAllLaunchers()
+    }, [])
     return (
-        <div>
+        <div className='home-page'>
+            <h1>all launchers page</h1>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>NAME</th>
+                    <th>CITY</th>
+                    <th>ROCKET TYPE</th>
+                    <th>LATITUDE</th>
+                    <th>LONGITUDE</th>
+                </tr>
+                {launchers?.map((item, index)=>
+                <tr key={index}>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.city}</td>
+                    <td>{item.rocketType}</td>
+                    <td>{item.latitude}</td>
+                    <td>{item.longitude}</td>
+                </tr>
+                )}
+            </table>
 
         </div>
     )
